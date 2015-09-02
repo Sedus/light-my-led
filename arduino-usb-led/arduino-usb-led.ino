44d8fb7301ec46814d654dab0d8ccbc90a743099
Pim Voeten <pim.voeten@gmail.com>
2015-09-02 23:55:54 +0200

    Arduino is working from Arduino IDE. Sends status updates and light up the LED on input

--- arduino-usb-led/arduino-usb-led.ino
+++ arduino-usb-led/arduino-usb-led.ino
@@ -1,62 +1,69 @@
 const byte outputLedPin = 13; // Target LED to light up
 const byte inputLedPin = 12; // Lights when data is received
-const int updateDelay = 500; // Sends signals every x milliseconds
+const int updateDelay = 1000; // Sends signals every x milliseconds
 unsigned long currentMillis = 0; // Current millis count
 unsigned long lastUpdateMillis = 0; // Last time a message was sent (used for message loop)
 unsigned long startTime = 0; // Amount of milliseconds since the LED started shining
-unsigned int seconds = 0; // Amount of seconds the LED should shine
+unsigned long seconds = 0; // Amount of seconds the LED should shine
 
 void setup() {
+  delay(1000);
   Serial.begin(115200);
+  Serial.setTimeout(500);
   pinMode(outputLedPin, OUTPUT);
   pinMode(inputLedPin, OUTPUT);
+  currentMillis = millis();
+  startTime = currentMillis;
 }
 
 void loop() {
   currentMillis = millis();
 
+  // Check input
+  getSecondsFromInput(digitalRead(outputLedPin) == HIGH, inputLedPin, seconds);
+
   // Turn on/off LED
-  if(seconds > 0) {
-    startTime = turnLedOnOff(outputLedPin, seconds);
-  }
-  
-  // Check input, but only if the amount of seconds have passed
-  if (seconds == 0) {
-    seconds = getSecondsFromInput(outputLedPin, inputLedPin);
-  }
+  turnLedOnOff(outputLedPin, seconds, startTime, currentMillis);
 
   // Send output message over serial
   if (currentMillis - lastUpdateMillis >= updateDelay) {
-    sendOutput(digitalRead(outputLedPin) == HIGH, currentMillis - startTime);
-    lastUpdateMillis = millis();
+    sendOutput(digitalRead(outputLedPin) == HIGH, currentMillis - startTime, seconds);
+    lastUpdateMillis = currentMillis;
   }
 }
 
-unsigned long turnLedOnOff(byte outputLedPin, unsigned int seconds) {
-  if (seconds > 0 && digitalRead(outputLedPin) == LOW) {
+void turnLedOnOff(byte outputLedPin, unsigned long &seconds, unsigned long &startTime, unsigned long currentMillis) {
+  if (seconds > 0 && currentMillis - startTime < seconds) {
+    if (digitalRead(outputLedPin) == LOW) {
+      startTime = millis();
+      Serial.println("start time set");
+    }
     digitalWrite(outputLedPin, HIGH);
-    return millis();
   } else {
     digitalWrite(outputLedPin, LOW);
-    return 0;
+    seconds = 0;
+    startTime = currentMillis;
   }
 }
 
-long getSecondsFromInput(byte outputLedPin, byte inputLedPin) {
+void getSecondsFromInput(boolean isOn, byte inputLedPin, unsigned long &seconds) {
   if (Serial.available() > 0) {
     digitalWrite(inputLedPin, HIGH);
-    return abs(Serial.parseInt()) * 1000;
+    seconds = Serial.parseInt() * 1000;
   } else {
     digitalWrite(inputLedPin, LOW);
   }
-  return 0;
 }
 
-void sendOutput(boolean on, unsigned long millis) {
-  String duration = "";
-  if(millis > 0) {
-    duration = ",\"duration\":" + millis;
+void sendOutput(boolean on, unsigned long ms, unsigned long seconds) {
+  String json = "{\"led\":{";
+  json += "\"status\":\"" + (String)(on ? "ON" : "OFF") + "\"";
+  if (on) {
+    json += ",\"duration\":" + (String)ms;
+    json += ",\"seconds\":" + (String)(seconds / 1000);
   }
-  Serial.println("{\"led\":{\"status:\"" + (String)(on ? "ON" : "OFF") + duration + "}}");
+  json += "}}";
+  Serial.println(json);
 }
+
 
